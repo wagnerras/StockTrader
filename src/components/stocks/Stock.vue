fe22fdf163004a6eb0b3cd2cba99a1c38aca5e20
unknown <wagnerras@gmail.com>
2021-02-20 15:45:45 -0300

    tela inicial

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,43 +1,70 @@
 <template>
- <v-flex class="pr-3 pb-3" xs12 md6 lg4>
-  <v-card class="green darken-3 white--text">
-    <v-card-title class="headline">
-      <strong>{{stock.name}} <small>(preço: {{stock.price}})</small></strong>
-    </v-card-title>
-  </v-card>
-  <v-card>
-    <v-container fill-height>
-      <v-text-field label="Quantidade" type="number" v-model.number="quantity"></v-text-field>
-      <v-btn class="green darken-3 white--text" 
-      :disabled="quantity <= 0 || !Number.isInteger(quantity)"
-      @click="buyStock">Comprar</v-btn>
-    </v-container>
-  </v-card>
- </v-flex>
+  <v-flex class="pr-3 pb-3" xs12 md6 lg4>
+    <v-card class="green darken-3 white--text">
+      <v-card-title class="headline">
+        <strong
+          >{{ stock.name }} <small>(preço: {{ stock.price }})</small></strong
+        >
+      </v-card-title>
+    </v-card>
+    <v-card>
+      <v-container fill-height>
+        <v-text-field
+          label="Quantidade"
+          type="number"
+          v-model.number="quantity"
+        ></v-text-field>
+        <v-btn
+          class="green darken-3 white--text"
+          :disabled="
+            quantity <= 0 || !Number.isInteger(quantity) || insufficientFunds
+          "
+          @click="buyStock"
+          >{{ insufficientFunds ? "Insuficiente" : "Comprar" }}</v-btn
+        >
+      </v-container>
+    </v-card>
+  </v-flex>
 </template>
 
 <script>
 export default {
-  props: ['stock'],
-  data(){
+  props: ["stock"],
+  data() {
     return {
-      quantity: 0
+      quantity: 0,
+    };
+  },
+  watch: {
+    quantity(){
+      if(this.quantity < 0) {
+        setTimeout(_ => {
+          this.quantity = 0
+        },1)
+      }
     }
   },
-  methods:{
-    buyStock(){
+  computed: {
+    funds() {
+      return this.$store.getters.funds;
+    },
+    insufficientFunds() {
+      return this.quantity * this.stock.price > this.funds;
+    },
+  },
+  methods: {
+    buyStock() {
       const order = {
         stockId: this.stock.id,
         stockPrice: this.stock.price,
-        quantity: this.quantity
-      }
-      console.log(order);
+        quantity: this.quantity,
+      };
+      this.$store.dispatch("buyStock", order);
       this.quantity = 0;
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
